be93efb2bc80fffe9d925021ffb13f0e8c06f89f
Unknown-Arda <125037095+Unknown-Arda@users.noreply.github.com>
2023-03-12 18:08:56 +0100

    drgdrg
    grg

---
+++ games/cookie/script.js
@@ -0,0 +1,14 @@
+const maxMoney = 50; //in cent
+
+document.getElementById("clicker").addEventListener("click", click);
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function click() {
+  const randomMoney = randomIntFromInterval(1, maxMoney);
+  moneyOutput = randomMoney * 0.01;
+  moneyOutput = moneyOutput.toFixed(2);
+  console.log(moneyOutput);
+}
