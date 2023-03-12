3ca36c8cb0ea44cbf5e344f68ae09bb71d72b713
Unknown-Arda <125037095+Unknown-Arda@users.noreply.github.com>
2023-03-12 14:16:56 +0100

    drztd
    hgczfg

---
+++ games/football/script.js
@@ -0,0 +1,37 @@
+var canvas = document.getElementById('canvas');
+
+canvas.width = 1000;
+canvas.height = 600;
+
+var ctx = canvas.getContext('2d');
+
+var x = 500, // Anfangs position x 
+    y = 300,
+    dx = 3.2, // x geschwindigkeit
+    dy = 0.3,
+    radius = 20;
+
+draw();
+
+function draw() {
+    window.requestAnimationFrame(draw)
+
+    ctx.clearRect(0,0,innerWidth, innerHeight)
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    ctx.fillStyle = 'white'
+    ctx.fill()
+
+    x += dx;
+    y += dy;
+
+    if(x + radius > 1000 || x - radius < 0) {
+        dx = -dx * 1.01;
+    }
+
+    if(y + radius > 600 || y - radius < 0) {
+        dy = -dy * 1.01;
+    }
+}
