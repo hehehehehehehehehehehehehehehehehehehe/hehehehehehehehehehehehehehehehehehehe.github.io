20d08caedcbbb9a0a550690a1419c71cb9096547
la-ser <77540680+la-ser@users.noreply.github.com>
2023-03-13 17:56:39 +0100

    1

--- games/cookie/script.js
+++ games/cookie/script.js
@@ -1,5 +1,4 @@
 let moneyValue = 0;
-<<<<<<< Updated upstream
 document.getElementById("hand").addEventListener("click", click);
 function click() {
   moneyProperty = Math.floor(Math.random() * 100);
@@ -49,54 +48,6 @@ function click() {
   } else {
     moneyReward.style.color = "#9d20c7";
     moneyReward.style.fontSize = 30 + "px";
-=======
-let rewardValue = 0;
-document.getElementById("clicker").addEventListener("click", click);
-function click() {
-  moneyProperty = Math.floor(Math.random() * 100);
-  if (moneyProperty < 21) {
-    moneyValue += .01;
-    rewardValue = .01;
-  }
-  if (20 < moneyProperty && moneyProperty < 51) {
-    moneyValue += .02;
-    rewardValue = .02;
-  }
-  if (50 < moneyProperty && moneyProperty < 71) {
-    moneyValue += .05;
-    rewardValue = .05;
-  }
-  if (70 < moneyProperty && moneyProperty < 86) {
-    moneyValue += .1;
-    rewardValue = .1;
-  }
-  if (85 < moneyProperty && moneyProperty < 96) {
-    moneyValue += .2;
-    rewardValue = .2;
-  }
-  if (95 < moneyProperty && moneyProperty < 101) {
-    moneyValue += .5;
-    rewardValue = .5;
-  }
-
-  const moneyReward = document.createElement("p")
-  moneyReward.innerText = "+" + rewardValue.toFixed(2);
-  moneyReward.id = "moneyReward"
-
-  moneyReward.style.position = "absolute";
-  moneyReward.style.left = Math.floor(Math.random() * (moneyRewardDiv.offsetWidth)) + "px";
-  moneyReward.style.top = Math.floor(Math.random() * (moneyRewardDiv.offsetHeight)) + "px";
-
-  if (rewardValue < .03) {
-    moneyReward.style.color = "white";
-    moneyReward.style.fontSize = 12 + "px";
-  } else if (rewardValue < .3) {
-    moneyReward.style.color = "lime";
-    moneyReward.style.fontSize = 16 + "px";
-  } else {
-    moneyReward.style.color = "#9d20c7";
-    moneyReward.style.fontSize = 28 + "px";
->>>>>>> Stashed changes
   }
 
   const randomIndex = Math.floor(Math.random() * 2);
