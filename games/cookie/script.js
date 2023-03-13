5ae828ba4985c8aeef0ade58ce00e50df5c233d9
Unknown-Arda <125037095+Unknown-Arda@users.noreply.github.com>
2023-03-13 17:34:06 +0100

    awdawd
    wdawd

--- games/cookie/script.js
+++ games/cookie/script.js
@@ -3,23 +3,58 @@ document.getElementById("clicker").addEventListener("click", click);
 function click() {
   moneyProperty = Math.floor(Math.random() * 100);
   if (moneyProperty < 21) {
-    moneyValue += 0.01;
+    moneyValue += .01;
+    rewardValue = .01;
+  } if (20 < moneyProperty && moneyProperty < 51) {
+    moneyValue += .02;
+    rewardValue = .02;
+  } if (50 < moneyProperty && moneyProperty < 71) {
+    moneyValue += .05;
+    rewardValue = .05;
+  } if (70 < moneyProperty && moneyProperty < 86) {
+    moneyValue += .1;
+    rewardValue = .1;
+  } if (85 < moneyProperty && moneyProperty < 96) {
+    moneyValue += .2;
+    rewardValue = .2;
+  } if (95 < moneyProperty && moneyProperty < 101) {
+    moneyValue += .5;
+    rewardValue = .5;
   }
-  if (20 < moneyProperty && moneyProperty < 51) {
-    moneyValue += 0.02;
+
+  const moneyReward = document.createElement("p")
+  moneyReward.innerText = "+" + rewardValue.toFixed(2);
+  moneyReward.id = "moneyReward"
+  moneyReward.style.position = "absolute";
+  moneyReward.style.left = Math.floor(Math.random() * (moneyRewardDiv.offsetWidth)) + "px";
+  moneyReward.style.top = Math.floor(Math.random() * (moneyRewardDiv.offsetHeight)) + "px";
+
+  if (rewardValue < .03) {
+    moneyReward.style.color = "white";
+    moneyReward.style.fontSize = 11 + "px";
+  } else if ( rewardValue < 0.06){
+    moneyReward.style.color = "lightgreen";
+    moneyReward.style.fontSize = 13 + "px";
+  } else if (rewardValue < .3) {
+    moneyReward.style.color = "lime";
+    moneyReward.style.fontSize = 19 + "px";
+  } else {
+    moneyReward.style.color = "#9d20c7";
+    moneyReward.style.fontSize = 30 + "px";
   }
-  if (50 < moneyProperty && moneyProperty < 71) {
-    moneyValue += 0.05;
+
+  const randomIndex = Math.floor(Math.random() * 2);
+  if (randomIndex === 0) {
+    moneyRewardDiv.appendChild(moneyReward);
+  } else {
+    moneyRewardDiv2.appendChild(moneyReward);
   }
-  if (70 < moneyProperty && moneyProperty < 86) {
-    moneyValue += 0.1;
-  }
-  if (85 < moneyProperty && moneyProperty < 96) {
-    moneyValue += 0.2;
-  }
-  if (95 < moneyProperty && moneyProperty < 101) {
-    moneyValue += 0.5;
-  }
+
+  setTimeout(() => {
+    moneyReward.remove();
+  }, 2000);
+
+  
 }
 
 // CUSTOM CURSOR
