0d3a5e44d00ba0abca906835d2c2df1a5fefa597
la-ser <77540680+la-ser@users.noreply.github.com>
2023-03-13 15:47:56 +0100

    new machine button

--- games/cookie/script.js
+++ games/cookie/script.js
@@ -2,18 +2,22 @@ let moneyValue = 0;
 document.getElementById("clicker").addEventListener("click", click);
 function click() {
   moneyProperty = Math.floor(Math.random() * 100);
-  if(moneyProperty < 21) {
+  if (moneyProperty < 21) {
     moneyValue += 0.01;
-  } if (20 < moneyProperty && moneyProperty < 51) {
-    moneyValue += 0.02;
-  } if (50 < moneyProperty && moneyProperty < 71) {
-    moneyValue += 0.05;
-  } if (70 < moneyProperty && moneyProperty < 86) {
-    moneyValue += 0.10;
-  } if (85 < moneyProperty && moneyProperty < 96) {
-    moneyValue += 0.20;
-  } if (95 < moneyProperty && moneyProperty < 101) {
-    moneyValue += 0.50;
   }
-  document.getElementById("moneyValueDisplay").innerHTML = "You have " + moneyValue.toFixed(2) + "€";
+  if (20 < moneyProperty && moneyProperty < 51) {
+    moneyValue += 0.02;
+  }
+  if (50 < moneyProperty && moneyProperty < 71) {
+    moneyValue += 0.05;
+  }
+  if (70 < moneyProperty && moneyProperty < 86) {
+    moneyValue += 0.1;
+  }
+  if (85 < moneyProperty && moneyProperty < 96) {
+    moneyValue += 0.2;
+  }
+  if (95 < moneyProperty && moneyProperty < 101) {
+    moneyValue += 0.5;
+  }
 }
