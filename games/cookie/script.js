2fb9d726b6673ae1af536910933fe10a2f8ceef8
Unknown-Arda <125037095+Unknown-Arda@users.noreply.github.com>
2023-03-14 14:35:43 +0100

    sd
    sd

--- games/cookie/script.js
+++ games/cookie/script.js
@@ -8,11 +8,11 @@ function click() {
   }
   if (20 < moneyProperty && moneyProperty < 51) {
     moneyValue += 0.02;
-    rewardValue = 0.02;
+    rewardValue = 0.02; 
   }
   if (50 < moneyProperty && moneyProperty < 71) {
     moneyValue += 0.05;
-    rewardValue = 0.05;
+    rewardValue = 0.05; 
   }
   if (70 < moneyProperty && moneyProperty < 86) {
     moneyValue += 0.1;
@@ -20,7 +20,7 @@ function click() {
   }
   if (85 < moneyProperty && moneyProperty < 96) {
     moneyValue += 0.2;
-    rewardValue = 0.2;
+    rewardValue = 0.2; 
   }
   if (95 < moneyProperty && moneyProperty < 101) {
     moneyValue += 0.5;
