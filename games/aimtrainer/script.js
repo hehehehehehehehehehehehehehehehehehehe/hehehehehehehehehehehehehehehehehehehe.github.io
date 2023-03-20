5f3262adf04c5be47685af4cce5840f9a26130f3
la-ser <77540680+la-ser@users.noreply.github.com>
2023-03-20 20:55:26 +0100

    Update script.js

--- games/aimtrainer/script.js
+++ games/aimtrainer/script.js
@@ -6,50 +6,55 @@ let timeleft = 20000;
 let gamegoeson = false;
 
 document.addEventListener("mousedown", function (event) {
-    if (event.target.id === "startDiv") {
-        start();
-      }
+  if (event.target.id === "startDiv") {
+    start();
+  }
 
-    if (event.target.id === "aimCursor") {
-      click();
-    }
+  if (event.target.id === "aimCursor") {
+    click();
+  }
 });
 
 function start() {
-    let startDiv = document.getElementById("startDiv");
-    startDiv.style.zIndex = "-1"
-    setTimeout(timerEnd, timeleft);
-    cursor.style.left = Math.floor(Math.random() * aimField.offsetWidth / 2) + "px";
-    cursor.style.top = Math.floor(Math.random() * aimField.offsetHeight / 2) + "px";
-    gamegoeson = true;
+  let startDiv = document.getElementById("startDiv");
+  startDiv.style.zIndex = "-1";
+  setTimeout(timerEnd, timeleft);
+  cursor.style.left =
+    Math.floor((Math.random() * aimField.offsetWidth) / 2) + "px";
+  cursor.style.top =
+    Math.floor((Math.random() * aimField.offsetHeight) / 2) + "px";
+  gamegoeson = true;
 }
 
 function click() {
-    score += 1;
-    cursor.style.left = Math.floor(Math.random() * aimField.offsetWidth / 2) + "px";
-    cursor.style.top = Math.floor(Math.random() * aimField.offsetHeight / 2) + "px";
+  score += 1;
+  cursor.style.left =
+    Math.floor((Math.random() * aimField.offsetWidth) / 2) + "px";
+  cursor.style.top =
+    Math.floor((Math.random() * aimField.offsetHeight) / 2) + "px";
 }
 
 function timerEnd() {
+  if (score > highscore) {
+    highscore = score;
+    document.getElementById("highscore").innerHTML = "Highscore: " + highscore;
+  }
 
-    if (score > highscore) {
-        highscore = score
-        document.getElementById("highscore").innerHTML = "Highscore: " + highscore;
-    }
-
-    const message = "Du hast " + score + " Punkte erreicht"
-    alert(message);
-    score = 0;
-    timeleft = 20000;
-    startDiv.style.zIndex = "2"
-    gamegoeson = false;
+  const message = "Du hast " + score + " Punkte erreicht";
+  alert(message);
+  score = 0;
+  timeleft = 20000;
+  startDiv.style.zIndex = "2";
+  gamegoeson = false;
 }
 
 setInterval(() => {
-    
-    if (gamegoeson == true) {
-        timeleft -= 4;
-    }
-    document.getElementById("time").innerHTML = "Time left: " + timeleft;
+  if (gamegoeson == true) {
+    timeleft -= 4;
+  }
 
+  timeleft2 = timeleft / 1000;
+
+  document.getElementById("time").innerHTML =
+    "Time left: " + timeleft2.toFixed(1);
 }, 1);
